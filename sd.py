3c2dd9c2fb1ec8f15fa998815433a1acbe4daa40
OM1484 <77450583+OM1484@users.noreply.github.com>
2022-02-08 19:29:29 +0530

    Add files via upload

---
+++ sd.py
@@ -0,0 +1,28 @@
+import csv
+import math
+f=open("class1.csv")
+file=csv.reader(f)
+df=list(file)
+df.pop(0)
+marks=[]
+for i in range(len(df)):
+    num=df[i][1]
+    marks.append(float(num))
+sum=0
+for i in marks:
+    sum=sum+i 
+mean=sum/len(marks)
+sq=[]
+for i in marks:
+    a=int(i)-mean
+    a=a**2
+    sq.append(a)
+add=0 
+for i in sq:
+    add=add+i
+result=add/(len(marks)-1)
+sd=math.sqrt(result)
+print(sd) 
+import statistics
+sd=statistics.stdev(marks)
+print(sd)
